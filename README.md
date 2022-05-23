c8aab1a1bf4fd1d5dc1aceedf3b25992f46152d8
Daniel Alejandro Fernandez <daf111@yahoo.com.ar>
2022-05-23 12:29:10 -0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# Tipos de triángulo
+Ingresar 3 datos y decir que clase de triangulo es (isósceles, escaleno o equilátero). Para formar un triángulo hay que tener en cuenta que la suma de sus dos lados inferiores tiene que ser mayor al lado superior.
---
+++ tipos-triangulos/Program.cs
@@ -0,0 +1,65 @@
+﻿using System;
+
+namespace tipos_triangulos
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] lados = cargaDeLados(); //Entrada de Datos
+
+            if (esTriangulo(lados)) //Validación
+            {
+                Console.WriteLine(tipoDeTriangulo(lados)); //Algoritmo y salida
+            }
+            else
+            {
+                Console.WriteLine("No ha ingresado un triángulo");
+            }
+        }
+
+        static int[] cargaDeLados()
+        {
+            return new int[] { 1, 2, 1 };
+        }
+
+        static void ordenarDeFormaDescendente(int[] lados)
+        {
+            for (int i = 0; i < (lados.Length - 1); i++)
+            {
+                for (int j = (i + 1); j < lados.Length; j++)
+                {
+                    if (lados[i] < lados[j])
+                    {
+                        int aux = lados[j];
+                        lados[j] = lados[i];
+                        lados[i] = aux;
+                    }
+                }
+            }
+        }
+        static bool esTriangulo(int[] lados)
+        {
+            ordenarDeFormaDescendente(lados);
+
+            return (lados[1] + lados[2]) > lados[0];
+        }
+
+        static string tipoDeTriangulo(int[] lados)
+        {
+            if (lados[0] == lados[1] && lados[1] == lados[2])
+            {
+                return "Es un triangulo equilatero";
+            }
+            else if (lados[0] == lados[1] || lados[1] == lados[2] || lados[0] == lados[2])
+            {
+                return "Es un triangulo isosceles";
+
+            }
+            else
+            {
+                return "Es un triangulo escaleno";
+            }
+        }
+    }
+}
